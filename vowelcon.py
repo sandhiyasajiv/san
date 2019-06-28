01f4495b25c348b5e57b71d0477e74abb4b90032
Yuvanshankar21 <52278651+Yuvanshankar21@users.noreply.github.com>
2019-06-28 14:10:55 +0530

    Create vowelcon.py

---
+++ vowelcon.py
@@ -0,0 +1,7 @@
+n=raw_input("")
+if n=='a'or n=='e'or n=='i'or n=='o'or n=='u' or n=='A'or n=='E'or n=='I'or n=='O'or n=='U':
+    print("Vowel")
+elif n=='z'or n=='b'or n=='t'or n=='h'or n=='g' or n=='Z'or n=='B'or n=='T'or n=='H'or n=='G': 
+    print("Consonant")
+else:
+      print("invalid")
