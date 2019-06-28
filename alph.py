5a10249522e963e8907981f005cea6fe0a0005c4
Yuvanshankar21 <52278651+Yuvanshankar21@users.noreply.github.com>
2019-06-28 15:05:05 +0530

    Create alph.py

---
+++ alph.py
@@ -0,0 +1,5 @@
+n=input("")
+if n=='a'or n=='b'or n=='c'or n=='d'or n=='e'or n=='f'or n=='g'or n=='h'or n=='i'or n=='j'or n=='k'or n=='l'or n=='m'or n=='n'or n=='o'or n=='p'or n=='q'or n=='r'or n=='s'or n=='t'or n=='u'or n=='v'or n=='w'or n=='x'or n=='y'or n=='z'or n=='A'or n=='B'or n=='C'or n=='D'or n=='E'or n=='F'or n=='G'or n=='H'or n=='I'or n=='J'or n=='K'or n=='L'or n=='M'or n=='N'or n=='O'or n=='P'or n=='R'or n=='S'or n=='t'or n=='U'or n=='V'or  n=='W'or n=='X'or n=='Y'or n=='Z':
+    print("Alphabet")
+else:
+      print("No")
