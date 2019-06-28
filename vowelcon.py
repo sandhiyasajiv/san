da8f0dd7f554d42d86313a7f52364bf13611105b
Yuvanshankar21 <52278651+Yuvanshankar21@users.noreply.github.com>
2019-06-28 14:29:31 +0530

    Update vowelcon.py

--- vowelcon.py
+++ vowelcon.py
@@ -1,4 +1,4 @@
-n=raw_input("")
+n=input("")
 if n=='a'or n=='e'or n=='i'or n=='o'or n=='u' or n=='A'or n=='E'or n=='I'or n=='O'or n=='U':
     print("Vowel")
 elif n=='z'or n=='b'or n=='t'or n=='h'or n=='g' or n=='Z'or n=='B'or n=='T'or n=='H'or n=='G': 
