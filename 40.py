80e7d2ba5c65f1137bfc4b5ba0fce2545389d566
Yuvanshankar21 <52278651+Yuvanshankar21@users.noreply.github.com>
2019-07-04 11:12:14 +0530

    Update 40.py

--- 40.py
+++ 40.py
@@ -3,12 +3,12 @@ n1 = 0
 n2 = 1
 count = 0
 if nterms <= 0:
-  print("0")
+  print(n1)
 elif nterms == 1:
-   print(n1)
+   print(n2)
 else:
    while count < nterms:
-       print(n1,end='  ')
+       print(n2,end='  ')
        nth = n1 + n2
        n1 = n2
        n2 = nth
