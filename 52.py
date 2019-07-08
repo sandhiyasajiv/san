ae6833db1bc4b2e5cb5a175cf6a29c2baf302dec
Yuvanshankar21 <52278651+Yuvanshankar21@users.noreply.github.com>
2019-07-08 11:43:06 +0530

    Create 52.py

---
+++ 52.py
@@ -0,0 +1,21 @@
+num=int(input())
+if num==1:
+  print("One")
+elif num==2:
+  print("Two")
+elif num==3:
+  print("Three")
+elif num==4:
+  print("Four")
+elif num==5:
+  print("Five")
+elif num==6:
+  print("Six")
+elif num==7:
+  print("Seven")
+elif num==8:
+  print("Eight")
+elif num==9:
+  print("Nine")
+elif num==10:
+  print("Ten")
