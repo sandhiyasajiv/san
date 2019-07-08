7052418a45f7b59b10f670035b4e22b3999741aa
Yuvanshankar21 <52278651+Yuvanshankar21@users.noreply.github.com>
2019-07-08 15:43:27 +0530

    Create pattern.py

---
+++ pattern.py
@@ -0,0 +1,22 @@
+l=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p']
+length=len(l)
+for i in range (0,1):
+  for j in range(0,1+i):
+    print(l[j],end=' ')
+  print('\n')
+for i in range (0,1):
+  for j in range(1,3+i):
+    print(l[j],end=' ')
+  print('\n')
+for i in range (0,1):
+  for j in range(3,6+i):
+    print(l[j],end=' ')
+  print('\n')
+for i in range (0,1):
+  for j in range(6,10+i):
+    print(l[j],end=' ')
+  print('\n')  
+for i in range (0,1):
+  for j in range(10,15):
+    print(l[j],end=' ')
+  print('\n')
