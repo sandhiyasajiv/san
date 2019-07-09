a720a303d004467187bc6609889a015980ee40fc
Yuvanshankar21 <52278651+Yuvanshankar21@users.noreply.github.com>
2019-07-09 14:06:42 +0530

    Update hun6.py

--- hun6.py
+++ hun6.py
@@ -1,11 +1,9 @@
-num=input()
-flag=0
-array=[int(k) for k in input().split()]
+num=int(input())
+array=list(map(int,input().split()))
 for i in array:
-    if(array.count(i)!=1):
-        f=1
-        break
-if(f==1):
-    print(i)
-else:
-    print("unique")
+  if array.count(i)>1:
+      print(i)
+      break
+  else:
+      print("unique")
+      break
